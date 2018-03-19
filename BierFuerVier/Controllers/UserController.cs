2dba47fb72378bdbf83a6f4ab8c5b5d430e1f824
Luca Dahinden <luca.dahinden@gmx.ch>
2018-03-19 10:42:58 +0100

    Added UserController with Login procedure

---
+++ BierFuerVier/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using BierFuerVier.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BierFuerVier.Controllers
+{
+    public class UserController : Controller
+    {
+        private DbAccess db;
+
+        public UserController()
+        {
+            db = new DbAccess();
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            User dbUser = db.User.SingleOrDefault(u => u.Email == user.Email);
+            if (dbUser != null && dbUser.Password == user.Password)
+            {
+                Session["User"] = user;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(user);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            db?.Dispose();
+        }
+    }
+}
