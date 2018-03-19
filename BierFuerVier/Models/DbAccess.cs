be189c883e8226bd98e0bcb21b91288f60ff04bb
uc <uc@UC>
2018-03-19 10:09:23 +0100

    Included EntityFramework and added Models

---
+++ BierFuerVier/Models/DbAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BierFuerVier.Models
+{
+    public class DbAccess : DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public DbSet<Beer> Beer { get; set; }
+    }
+}
